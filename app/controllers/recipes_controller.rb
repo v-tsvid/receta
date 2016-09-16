0a6406481b2bfb7a49b91ece60a6700ad7853aec
v-tsvid <v.tsvid@i.ua>
2016-09-16 19:17:58 +0300

    show recipe feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,14 @@
 class RecipesController < ApplicationController
+  
   def index
     @recipes = if params[:keywords]
       Recipe.where('name ilike ?',"%#{params[:keywords]}%")
     else
       []
     end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create!(name: 'Baked Potato w/ Cheese', 
+           instructions: "nuke for 20 minutes")
+
+Recipe.create!(name: 'Baked Brussel Sprouts',
+       instructions: 'Slather in oil, and roast on high heat for 20 minutes')
