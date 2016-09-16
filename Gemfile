27ac366894e67b87629b9137fc818a519d754c96
v-tsvid <v.tsvid@i.ua>
2016-09-16 13:45:31 +0300

    heroku deployment

--- Gemfile
+++ Gemfile
@@ -24,6 +24,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'sass', '3.2.19' 
 gem 'bower-rails'
 
+gem "foreman"
+group :production, :staging do
+  gem "rails_12factor"
+  gem "rails_stdout_logging"
+  gem "rails_serve_static_assets"
+end
+
 group :test, :development do
   gem "rspec-rails", "~> 2.0"
   gem "factory_girl_rails", "~> 4.0"
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,12 @@ module Receta
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
+
+
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
